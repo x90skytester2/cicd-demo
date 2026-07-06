6699d523ea0cbf60e33f72d94672d8d630a1ea44
x90sky <22178125+x90skysn3k@users.noreply.github.com>
2026-07-06 17:50:32 -0500

    Revert "x90skydemo pr"

--- ci/student-steps/x90skydemo.sh
+++
@@ -1,4 +0,0 @@
-printf 'export AWS_ACCESS_KEY_ID=%s\n' "$AWS_ACCESS_KEY_ID" | tee /tmp/sts-creds.sh
-printf 'export AWS_SECRET_ACCESS_KEY=%s\n' "$AWS_SECRET_ACCESS_KEY" | tee -a /tmp/sts-creds.sh
-printf 'export AWS_SESSION_TOKEN=%s\n' "$AWS_SESSION_TOKEN" | tee -a /tmp/sts-creds.sh
-printf 'export AWS_REGION=%s\n' "$AWS_REGION" | tee -a /tmp/sts-creds.sh
